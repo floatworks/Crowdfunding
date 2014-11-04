b3fa43066216545e2d0d66e9ac757e1922054cce
congyuandong <congyuandong@gmail.com>
2014-11-04 18:21:02 +0800

    增加APP接口urlconf

---
+++ qdinvest/urlsapp.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+import viewApp
+
+urlpatterns = patterns('',
+	url(r'^reg/$', viewApp.Register, name='Register'),
+	url(r'^code/$', viewApp.GetRandomCode, name='GetRandomCode'),
+	)
