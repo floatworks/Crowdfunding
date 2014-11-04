8032015b43d033299cb1a44959f841b117aefdb0
congyuandong <congyuandong@gmail.com>
2014-11-04 18:36:18 +0800

    修改wsgi文件

--- Crowdfunding/wsgi.py
+++ Crowdfunding/wsgi.py
@@ -8,7 +8,19 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Crowdfunding.settings")
+
+import sys
+
+
+apache_configuration= os.path.dirname(__file__)
+project = os.path.dirname(apache_configuration)
+workspace = os.path.dirname(project)
+
+sys.path.append(apache_configuration)
+sys.path.append(project)
+sys.path.append(workspace)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "Crowdfunding.settings"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
