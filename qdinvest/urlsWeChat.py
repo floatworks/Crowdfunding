40fd5ae3c6fca9804dd41b170455e6c625fc32bf
zttonly <zhangtingting8023@gmail.com>
2014-11-22 13:57:01 +0800

    增加详情页项目介绍中重要提示等文字调用及格式

--- qdinvest/urlsWeChat.py
+++ qdinvest/urlsWeChat.py
@@ -3,5 +3,6 @@ import viewWechat
 
 urlpatterns = patterns('',
 	url(r'^$', viewWechat.Index, name='Index'),
-	url(r'^index/$', viewWechat.Index, name='Index'),
+	url(r'^index/(.+)$', viewWechat.Index, name='Index'),
+	url(r'^pro_detail/(.+)$', viewWechat.Pro_detail, name='Pro_detail'),
 	)
--- qdinvest/viewWechat.py
+++ qdinvest/viewWechat.py
@@ -3,8 +3,28 @@ from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse,HttpResponseRedirect
+from django.http import Http404
 
-def Index(request):
+from models import *
+
+def Index(request,id):
 	context = RequestContext(request)
 	context_dict = {}
+	try:
+		STOCK_obj = STOCK.objects.get(id__exact = id)
+		context_dict['stock'] = STOCK_obj
+	except STOCK.DoesNotExist:
+		raise Http404
+	print context_dict
 	return render_to_response('wechat/index.html',context_dict,context)
+
+def Pro_detail(request,id):
+	context = RequestContext(request)
+	context_dict = {}
+	try:
+		STOCK_obj = STOCK.objects.get(id__exact = id)
+		context_dict['stock'] = STOCK_obj
+	except STOCK.DoesNotExist:
+		raise Http404
+	print context_dict
+	return render_to_response('wechat/pro-details.html',context_dict,context)
