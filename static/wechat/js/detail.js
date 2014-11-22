cea6fb3ad889ca675f5398853d63d482a3c6252b
congyuandong <congyuandong@gmail.com>
2014-11-23 00:42:26 +0800

    修改视频大小

--- static/wechat/js/detail.js
+++ static/wechat/js/detail.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 	//加载保利视频
 	if($("#plv_video").length>0){
 		var player = polyvObject('#plv_video').videoPlayer({
-		'width':'600',
-		'height':'337',
+		'width':'100%',
+		'height':'200',
 		'vid' : $("#plv_video").attr('data')
 		});
 	}else{
