6687e1b14a42849c1b8c2798f726b404bb07547a
zttonly <zhangtingting8023@gmail.com>
2014-11-25 20:42:40 +0800

    调整轮播速度

--- static/wechat/js/plug.js
+++ static/wechat/js/plug.js
@@ -26,80 +26,80 @@
 		var order_by = 'ASC';
 		//初始化
 		var init = function() {
-				if (!wrapper.size()) return false;
+			if (!wrapper.size()) return false;
 
-				//li_height = lis.first().height();
-				//li_width = lis.first().width();
+			//li_height = lis.first().height();
+			//li_width = lis.first().width();
 
-				/*****自适应******/
-				li_height = firstPic.height();
-				li_width = firstPic.width();
-				w_width = $(window).width();
-				li_height = w_width * li_height / li_width;
-				li_width = w_width;
+			/*****自适应******/
+			li_height = firstPic.height();
+			li_width = firstPic.width();
+			w_width = $(window).width();
+			li_height = w_width * li_height / li_width;
+			li_width = w_width;
 
-				liPic.css({
-					width: li_width + 'px',
-					height: li_height + 'px'
+			liPic.css({
+				width: li_width + 'px',
+				height: li_height + 'px'
+			});
+			/*****自适应******/
+
+			wrapper.css({
+				width: li_width + 'px',
+				height: li_height + 'px'
+			});
+			//lis.css({width: li_width+'px', height:li_height+'px'});//ADD.JENA.201207051027
+
+			if (settings.direction == 'left') {
+				ul.css('width', li_num * li_width + 'px');
+			} else {
+				ul.css('height', li_num * li_height + 'px');
+			}
+			ul.find('li:eq(' + settings.startIndex + ')').addClass('active');
+
+			if (!settings.hideBottomBar) { //ADD.JENA.201208090859
+				var tips = $('<div class="tips"></div>').css('opacity', 0.6).appendTo(wrapper);
+				//增加标题
+				// var title = $('<div class="title"></div>').html(function(){
+				// 	var active = ul.find('li.active').find('a'), text = active.attr('title'), href = active.attr('href');
+				// 	return $('<a>').attr('href', href).text(text);
+				// }).appendTo(tips);
+				var nums = $('<div class="nums"></div>').hide().appendTo(tips);
+				lis.each(function(i, n) {
+					var a = $(n).find('a'),
+						text = a.attr('title'),
+						href = a.attr('href'),
+						css = '';
+					i == settings.startIndex && (css = 'active');
+					$('<a>').attr('href', href).text(text).addClass(css).css('borderRadius', settings.clickBarRadius + 'px').mouseover(function() {
+						$(this).addClass('active').siblings().removeClass('active');
+						ul.find('li:eq(' + $(this).index() + ')').addClass('active').siblings().removeClass('active');
+						start();
+						stop();
+					}).appendTo(nums);
 				});
-				/*****自适应******/
 
-				wrapper.css({
-					width: li_width + 'px',
-					height: li_height + 'px'
-				});
-				//lis.css({width: li_width+'px', height:li_height+'px'});//ADD.JENA.201207051027
-
-				if (settings.direction == 'left') {
-					ul.css('width', li_num * li_width + 'px');
-				} else {
-					ul.css('height', li_num * li_height + 'px');
-				}
-				ul.find('li:eq(' + settings.startIndex + ')').addClass('active');
-
-				if (!settings.hideBottomBar) { //ADD.JENA.201208090859
-					var tips = $('<div class="tips"></div>').css('opacity', 0.6).appendTo(wrapper);
-					//增加标题
-					// var title = $('<div class="title"></div>').html(function(){
-					// 	var active = ul.find('li.active').find('a'), text = active.attr('title'), href = active.attr('href');
-					// 	return $('<a>').attr('href', href).text(text);
-					// }).appendTo(tips);
-					var nums = $('<div class="nums"></div>').hide().appendTo(tips);
-					lis.each(function(i, n) {
-						var a = $(n).find('a'),
-							text = a.attr('title'),
-							href = a.attr('href'),
-							css = '';
-						i == settings.startIndex && (css = 'active');
-						$('<a>').attr('href', href).text(text).addClass(css).css('borderRadius', settings.clickBarRadius + 'px').mouseover(function() {
-							$(this).addClass('active').siblings().removeClass('active');
-							ul.find('li:eq(' + $(this).index() + ')').addClass('active').siblings().removeClass('active');
-							start();
-							stop();
-						}).appendTo(nums);
-					});
-
-					if (settings.hideClickBar) { //ADD.JENA.201206300847
-						tips.hover(function() {
-							nums.animate({
-								top: '0px'
-							}, 'fast');
-						}, function() {
-							nums.animate({
-								top: tips.height() + 'px'
-							}, 'fast');
-						});
-						nums.show().delay(2000).animate({
+				if (settings.hideClickBar) { //ADD.JENA.201206300847
+					tips.hover(function() {
+						nums.animate({
+							top: '0px'
+						}, 'fast');
+					}, function() {
+						nums.animate({
 							top: tips.height() + 'px'
 						}, 'fast');
-					} else {
-						nums.show();
-					}
+					});
+					nums.show().delay(2000).animate({
+						top: tips.height() + 'px'
+					}, 'fast');
+				} else {
+					nums.show();
 				}
-
-				lis.size() > 1 && start();
 			}
-			//开始轮播
+
+			lis.size() > 1 && start();
+		}
+
 		var start = function() {
 			var active = ul.find('li.active'),
 				active_a = active.find('a');
@@ -121,28 +121,22 @@
 
 			ul.stop().animate(param, settings.duration * 1000, settings.easing, function() {
 				active.removeClass('active');
-				if (order_by == 'ASC') {
-					if (active.next().size()) {
-						active.next().addClass('active');
-					} else {
-						order_by = 'DESC';
-						active.prev().addClass('active');
-					}
-				} else if (order_by == 'DESC') {
-					if (active.prev().size()) {
-						active.prev().addClass('active');
-					} else {
-						order_by = 'ASC';
-						active.next().addClass('active');
-					}
+				if (active.next().size()) {
+					active.next().addClass('active');
+				} else {
+					lis.first().addClass('active');
 				}
 			});
-			wrapper.data('timeid', window.setTimeout(start, settings.delay * 1000));
+			//wrapper.data('timeid', window.setTimeout(start, settings.delay * 1000));
 		};
+		wrapper.data('timeid', window.setInterval(start, settings.delay * 1000));
+
 		//停止轮播
 		var stop = function() {
-			window.clearTimeout(wrapper.data('timeid'));
+			window.clearInterval(wrapper.data('timeid'));
+			//window.clearTimeout(wrapper.data('timeid'));
 		};
+
 		// //鼠标经过事件
 		// wrapper.hover(function(){
 		// 	stop();
@@ -169,12 +163,11 @@
 				if (active.next().size()) {
 					active.next().addClass('active');
 				} else {
-					order_by = 'DESC';
 					active.addClass('active');
-				//	alert(index + ' ' + offset);
 				}
 			});
-			start();
+			//start();
+			wrapper.data('timeid', window.setInterval(start, settings.delay * 1000));
 		});
 
 		wrapper.on("swiperight", function() {
@@ -186,7 +179,7 @@
 			param = {
 				'left': offset + 'px'
 			};
-		//	alert(active + ' ' + index + ' ' + offset);
+
 			wrapper.find('.nums').find('a:eq(' + index + ')').addClass('active').siblings().removeClass('active');
 			wrapper.find('.title').find('a').attr('href', active_a.attr('href')).text(active_a.attr('title'));
 			ul.stop().animate(param, settings.duration * 1000, settings.easing, function() {
@@ -194,12 +187,11 @@
 				if (active.prev().size()) {
 					active.prev().addClass('active');
 				} else {
-					order_by = 'ASC';
 					active.addClass('active');
-				//	alert(index + ' ' + offset);
 				}
 			});
-			start();
+			//start();
+			wrapper.data('timeid', window.setInterval(start, settings.delay * 1000));
 		});
 
 		//首张图片加载完毕后执行初始化
--- static/wechat/js/qingda.js
+++ static/wechat/js/qingda.js
@@ -16,7 +16,7 @@ $(document).on("pageinit", "#index", function() {
 
 	if ($("#carousel").length > 0) {
 		$('#carousel').slideBox({
-			duration: 0.3, //滚动持续时间，单位：秒
+			duration: 0.5, //滚动持续时间，单位：秒
 			easing: 'linear', //swing,linear//滚动特效
 			delay: 5, //滚动延迟时间，单位：秒
 			hideClickBar: false, //不自动隐藏点选按键
