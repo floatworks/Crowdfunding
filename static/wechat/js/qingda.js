6d510aba387341be1faccb98f436e0233956cee2
zttonly <zhangtingting8023@gmail.com>
2014-11-25 15:56:43 +0800

    修改主页导航，轮播及详情页

--- static/wechat/js/qingda.js
+++ static/wechat/js/qingda.js
@@ -1,25 +1,30 @@
-$(document).ready(function() {
+$(document).on("pageinit", "#index", function() {
 	/*进度条*/
-	$(".progressbar").each(function(){
-		var pb = $(this).attr("data");
-	if(pb <= 50){
-		$(this).addClass("less");
-		
-	}else{
-		$(this).addClass("more");
-	}
-	$(this).find(".num").text(pb+'%');
-	$(this).find(".bar").css("width",pb+'%');
-	})
+	if ($(".progressbar").length > 0) {
+		$(".progressbar").each(function() {
+			var pb = $(this).attr("data");
+			if (pb <= 50) {
+				$(this).addClass("less");
 
-	/*轮播*/
-	$('#carousel').slideBox({
+			} else {
+				$(this).addClass("more");
+			}
+			$(this).find(".num").text(pb + '%');
+			$(this).find(".bar").css("width", pb + '%');
+		})
+	}
+
+	if ($("#carousel").length > 0) {
+		$('#carousel').slideBox({
 			duration: 0.3, //滚动持续时间，单位：秒
 			easing: 'linear', //swing,linear//滚动特效
 			delay: 5, //滚动延迟时间，单位：秒
 			hideClickBar: false, //不自动隐藏点选按键
 			clickBarRadius: 10
 		});
+	}
+	/*轮播*/
 
-	
+
+
 });
