20d36a819493eecf4cce408653dc9f7217429f65
zttonly <zhangtingting8023@gmail.com>
2014-11-27 21:36:19 +0800

    项目详情相关页面

--- static/wechat/js/qingda.js
+++ static/wechat/js/qingda.js
@@ -14,6 +14,7 @@ $(document).on("pageinit", "#index", function() {
 		})
 	}
 
+	/*轮播*/
 	if ($("#carousel").length > 0) {
 		$('#carousel').slideBox({
 			duration: 0.5, //滚动持续时间，单位：秒
@@ -23,8 +24,35 @@ $(document).on("pageinit", "#index", function() {
 			clickBarRadius: 10
 		});
 	}
-	/*轮播*/
 
+});
 
+var co_cover = $('.co_covers');
+$(document).on("pageshow", "#pro_detail", function() {
 
+	$.mobile.buttonMarkup.hoverDelay = "false";
+	//alert(co_cover.height());
+	//alert(co_cover.find('img').height());
+	if (co_cover.length > 0) {
+
+		$('#base_info').css('margin-top', co_cover.height() );
+		$(".progress .bar").css("width", $(".progressbar").attr("data") + '%');
+	}
+
+});
+
+$(".pro_detail #tabs").delegate(".ui-tabs-anchor", "click", function() {
+
+	if ($(this).attr('id') == "ui-id-1") {
+		$('#base_info').css('margin-top', co_cover.height() );
+		$(".progress .bar").css("width", $(".progressbar").attr("data") + '%');
+		co_cover.css('display', 'block');
+	} else {
+		$('#base_info').css('margin-top', 0);
+		co_cover.css('display', 'none');
+	}
+});
+
+$(".follow").delegate(".follow-heart", "click", function() {
+	$(this).toggleClass('active');
 });
