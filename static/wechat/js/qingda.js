1b28ea2bb87a92063cb2301659057f296f4d8405
zhangtingting <zhangtingting8023@gmail.com>
2014-12-08 17:35:59 +0800

    我的账户，全屏

--- static/wechat/js/qingda.js
+++ static/wechat/js/qingda.js
@@ -31,7 +31,7 @@ var co_cover = $('.co_covers');
 $(document).on("pageshow", "#pro_detail", function() {
 
 	$.mobile.buttonMarkup.hoverDelay = "false";
-	$('.pro_detail .ui-tabs').css('padding-top',$('.relative > .ui-grid-b').height()+$('.relative > .ui-grid-solo').height());
+	$('.pro_detail .ui-tabs').css('padding-top', $('.relative > .ui-grid-b').height() + $('.relative > .ui-grid-solo').height());
 	if (co_cover.length > 0) {
 		//$('#base_info').css('margin-top', co_cover.height());
 		$(".progress .bar").css("width", $(".progressbar").attr("data") + '%');
@@ -60,10 +60,10 @@ $(document).on("pageinit", "#pro_detail", function() {
 		min_price = $(this).attr('min');
 		if (price == null || price == '') {
 			$("#input-money").val('');
-			popDialog('#investDialog','认购金额不能为空');
+			popDialog('#investDialog', '认购金额不能为空');
 			return;
-		}else if(parseFloat(price) < parseFloat(min_price)){
-			popDialog('#investDialog','认购金额不能少于认购起点');
+		} else if (parseFloat(price) < parseFloat(min_price)) {
+			popDialog('#investDialog', '认购金额不能少于认购起点');
 			return;
 		}
 		$.post("/w/invest/", {
@@ -74,9 +74,9 @@ $(document).on("pageinit", "#pro_detail", function() {
 			function(data, status) {
 				if (data.status == -1) {
 					window.location.href = "/w/login/";
-				}else if(data.status == 1){
-					popDialog('#investDialog','认购成功');
-					$('#invest_count').text(parseInt($('#invest_count').text())+1);
+				} else if (data.status == 1) {
+					popDialog('#investDialog', '认购成功');
+					$('#invest_count').text(parseInt($('#invest_count').text()) + 1);
 				}
 			});
 		$("#input-money").val('');
@@ -88,11 +88,11 @@ $(document).on("pageinit", "#feedback", function() {
 	$('#feedback_btn').click(function() {
 		mail = $("#fb_email").val().trim();
 		content = $("#fb_textarea").val().trim();
-		if(!mail.match($.regexpCommon('email'))){
-			popDialog('#feedbackDialog','邮箱地址不合法');
+		if (!mail.match($.regexpCommon('email'))) {
+			popDialog('#feedbackDialog', '邮箱地址不合法');
 			return false;
-		}else if(content == null || content == ''){
-			popDialog('#feedbackDialog','反馈内容不能为空');
+		} else if (content == null || content == '') {
+			popDialog('#feedbackDialog', '反馈内容不能为空');
 			return false;
 		}
 
@@ -114,7 +114,7 @@ $(document).on("pageinit", "#reg", function() {
 	$('#getCode').click(function() {
 		tel = $("#u_tel").val().trim();
 		if (!tel.match($.regexpCommon('phoneNumberZN'))) {
-			popDialog('#popupDialog','手机号码格式不正确');
+			popDialog('#popupDialog', '手机号码格式不正确');
 			return;
 		}
 		$.get("/app/code/", {
@@ -122,10 +122,10 @@ $(document).on("pageinit", "#reg", function() {
 				'u_tel': tel
 			},
 			function(data, status) {
-				if(data.status == 2){
-					popDialog('#popupDialog','该手机号码已经被注册');
-				}else if (data.status == 1) {
-					popDialog('#popupDialog','验证码发送成功，请注意查收');
+				if (data.status == 2) {
+					popDialog('#popupDialog', '该手机号码已经被注册');
+				} else if (data.status == 1) {
+					popDialog('#popupDialog', '验证码发送成功，请注意查收');
 				}
 			});
 	});
@@ -133,55 +133,55 @@ $(document).on("pageinit", "#reg", function() {
 
 $(document).on("pageshow", "#login", function() {
 	var status = $.getUrlParam('s');
-	if(status == 'reg#' || status == 'reg'){
-		popDialog('#loginDialog','注册成功，请登录');
-	}else if(status == 'err' || status == 'err#'){
-		popDialog('#loginDialog','用户名或密码错误');
+	if (status == 'reg#' || status == 'reg') {
+		popDialog('#loginDialog', '注册成功，请登录');
+	} else if (status == 'err' || status == 'err#') {
+		popDialog('#loginDialog', '用户名或密码错误');
 	}
 });
 
 $(document).on("pageshow", "#reg", function() {
 	var status = $.getUrlParam('s');
-	if(status == 2){
-		popDialog('#popupDialog','用户名已存在');
-	}else if(status == 3){
-		popDialog('#popupDialog','该手机号码已被注册');
-	}else if(status == 4){
-		popDialog('#popupDialog','验证码错误');
+	if (status == 2) {
+		popDialog('#popupDialog', '用户名已存在');
+	} else if (status == 3) {
+		popDialog('#popupDialog', '该手机号码已被注册');
+	} else if (status == 4) {
+		popDialog('#popupDialog', '验证码错误');
 	}
 });
 //注册校验
-function checkReg(){
+function checkReg() {
 	u_name = $("#u_name").val().trim();
 	u_pwd = $("#u_pwd").val().trim();
 	cpw = $("#cpw").val().trim();
 	tel = $("#u_tel").val().trim();
 	code = $("#code").val().trim();
-	if(!u_name.match($.regexpCommon('username'))){
-		popDialog('#popupDialog','请输入6至20位用户名');
+	if (!u_name.match($.regexpCommon('username'))) {
+		popDialog('#popupDialog', '请输入6至20位用户名');
 		return false;
-	}else if(!u_pwd.match($.regexpCommon('pwd'))){
-		popDialog('#popupDialog','请输入6至16位密码');
+	} else if (!u_pwd.match($.regexpCommon('pwd'))) {
+		popDialog('#popupDialog', '请输入6至16位密码');
 		return false;
-	}else if(u_pwd != cpw){
-		popDialog('#popupDialog','两次密码不同');
+	} else if (u_pwd != cpw) {
+		popDialog('#popupDialog', '两次密码不同');
 		return false;
-	}else if(!tel.match($.regexpCommon('phoneNumberZN'))){
-		popDialog('#popupDialog','手机号码格式不正确');
+	} else if (!tel.match($.regexpCommon('phoneNumberZN'))) {
+		popDialog('#popupDialog', '手机号码格式不正确');
 		return false;
-	}else if(!code.match($.regexpCommon('code'))){
-		popDialog('#popupDialog','验证码为6位数字');
+	} else if (!code.match($.regexpCommon('code'))) {
+		popDialog('#popupDialog', '验证码为6位数字');
 		return false;
-	}else if($('#agreement').attr('data-cacheval') != 'false'){
-		popDialog('#popupDialog','请阅读并确认用户服务协议');
+	} else if ($('#agreement').attr('data-cacheval') != 'false') {
+		popDialog('#popupDialog', '请阅读并确认用户服务协议');
 		return false;
 	}
 	return true;
 }
 
 //Dialog弹出
-function popDialog(dialogID,text){
-	$(dialogID+" h3").text(text);
+function popDialog(dialogID, text) {
+	$(dialogID + " h3").text(text);
 	$(dialogID).popup('open');  
 }
 
@@ -232,3 +232,22 @@ $("#feedback #fb_textarea").keyup(function() {
 	(($(this).val().length) >= 80) ? ($("#ta_num").text('80/80')) : ($("#ta_num").text($(this).val().length + '/80'));
 
 })
+
+
+$(document).on("pageshow", "#personal", function() {
+	var w_width = $(window).height();
+	var header = $('#personal .ui-header'),
+		footer = $('#personal .ui-footer'),
+		solo = $('#personal .ui-content .ui-grid-solo'),
+		grid_a = $('#personal .ui-content .ui-grid-a');
+	var gap = (w_width - header.height() - footer.height() - solo.height()*2 - grid_a.height()*3) / 16;
+	solo.find('.ui-block-a').css({"padding-top": 2*gap,"padding-bottom": gap});
+	grid_a.css({"padding-top": gap,"padding-bottom": 2*gap});
+	$('#personal .ui-content .subscribe').css("padding-top", 2*gap);
+	// alert(w_width + ' ' + header.height() + ' ' + footer.height() + ' ' + solo.height() + ' ' + grid_a.height());
+	// alert(w_width - header.height() - footer.height() - solo.height() - grid_a.height());
+	// alert(gap);
+	// alert((w_width - header.height() - footer.height() - solo.height() - grid_a.height()) / 16);
+
+
+});
